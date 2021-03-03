3f3850797548773d2230db05be16004292029fb1
arana2743 <abhisekrana777@gmail.com>
2021-03-03 15:51:28 +0530

    data-types in Kotlin

---
+++ src/IntegerTypes.kt
@@ -0,0 +1,28 @@
+// In kotlin below are integer types:
+// Byte (8-bits size)
+// Short (16-bits size)
+// Int (32-bits size)
+// Long (64-bits size)
+
+// And Float types are:
+// Float (32-bits size)
+// Double (64-bits size)
+
+fun main() {
+    val myByte: Byte = -126
+    val myShort: Short = 125
+    val myInt: Int = -123456789
+    val myLong: Long = 39_812_309_487_120_300
+
+    println(myByte)
+    println(myShort)
+    println(myInt)
+    println(myLong)
+
+    val myFloat: Float = 13.689F
+    val myDouble: Double = 12456.49423476
+
+    println(myFloat)
+    println(myDouble)
+
+}
---
+++ src/OtherDataTypes.kt
@@ -0,0 +1,16 @@
+fun main() {
+    
+    // boolean
+    var checkFlag: Boolean = true
+    println(checkFlag)
+
+    // char
+    var singleChar: Char = 'A'
+    var digitChar: Char = '1'
+    println(singleChar)
+    println(digitChar)
+
+    var str: String = "String One"
+    println(str)
+
+}
