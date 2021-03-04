adb274fad3b1f547f6fd0ba892e128932894bcbf
arana2743 <abhisekrana777@gmail.com>
2021-03-04 22:42:42 +0530

    while and do-while loop in Kotlin

---
+++ src/WhileBlock.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var x = 1
+
+    // while loop
+    while (x<=10) {
+        print("$x ")
+        x++
+    }
+    println("\nWhile loop is done")
+
+    // do-while loop
+    var y = 10
+    do {
+        println("$x")
+    } while (y<5)
+    println("Do-while loop is done") 
+}
